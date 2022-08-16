b09f2145d0600ce832a222ecd1503c3baeb076f3
TNX0H <dimanmityan@yandex.ru>
2022-08-16 21:22:22 +0300

    2.3.1

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,14 +1,12 @@
 package web.dao;
 
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Component
-@Transactional
 public class UserDaoImpl implements UserDAO {
 
     @PersistenceContext
